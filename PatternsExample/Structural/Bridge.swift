5214bc4847fbdb5be90921d7dc6a030e71320e3c
Maria Khamitsevich <dragunova.maria97@gmail.com>
2023-01-26 15:59:01 +0100

    add bridge

--- PatternsExample/Structural/Bridge.swift
+++ PatternsExample/Structural/Bridge.swift
@@ -1,8 +1,37 @@
-//
-//  Bridge.swift
-//  PatternsExample
-//
-//  Created by Maria Khamitsevich on 26/01/2023.
-//
-
 import Foundation
+
+protocol ProductProtocol {
+    var name: String { get set }
+    var quality: Int { get set }
+}
+
+protocol DeliveryProtocol: AnyObject {
+    func deliverProduct(product: ProductProtocol)
+}
+
+struct Chocolate: ProductProtocol {
+    var name: String
+    var quality: Int
+}
+
+class Delivery: DeliveryProtocol {
+    private let client: ChocolateFabric
+    
+    init(client: ChocolateFabric) {
+        self.client = client
+        client.delivery = self
+    }
+    
+    func deliverProduct(product: ProductProtocol) {
+        print("\(product.name) delivered")
+    }
+}
+
+final class ChocolateFabric {
+    weak var delivery: DeliveryProtocol?
+    
+    func deliverChocolate(withName: String, inQuality: Int) {
+        let chocolate = Chocolate(name: withName, quality: inQuality)
+        delivery?.deliverProduct(product: chocolate)
+    }
+}
